d431313b4d67e1eb2eab6ccdf80bbc5689c2e20c
Zehra Seren <fatmazehraseren@gmail.com>
2025-01-20 06:39:21 +0300

    Initialize migration

---
+++ SignalR.DataAccessLayer/Concrete/SignalRContext.cs
@@ -0,0 +1,32 @@
+﻿using SignalR.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace SignalR.DataAccessLayer.Concrete
+{
+    public class SignalRContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=Zehra;initial catalog=RestaurantDb;integrated security=true;TrustServerCertificate=True");
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Discount> Discounts { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasColumnType("decimal(18,2)");
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
