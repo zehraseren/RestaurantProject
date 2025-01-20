5ac6acfe846deba972963920325a22d88bf9996f
Zehra Seren <fatmazehraseren@gmail.com>
2025-01-20 06:34:31 +0300

    Add entity classes

---
+++ SignalR.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace SignalR.EntityLayer.Concrete
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+    }
+}
