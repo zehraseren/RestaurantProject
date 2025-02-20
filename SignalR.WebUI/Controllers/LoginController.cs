a74c3721f59019293e5b27caaa3adab02a102c01
Zehra Seren <fatmazehraseren@gmail.com>
2025-02-21 02:36:36 +0300

    Initial implementation of Mail

--- SignalR.WebUI/Controllers/LoginController.cs
+++ SignalR.WebUI/Controllers/LoginController.cs
@@ -32,5 +32,11 @@ namespace SignalR.WebUI.Controllers
             }
             return View();
         }
+
+        public async Task<IActionResult> LogOut()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Login");
+        }
     }
 }
---
+++ SignalR.WebUI/Controllers/MailController.cs
@@ -0,0 +1,43 @@
+﻿using MimeKit;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.WebUI.Dtos.MailDtos;
+
+namespace SignalR.WebUI.Controllers
+{
+    public class MailController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(CreateMailDto cmdto)
+        {
+            MimeMessage message = new MimeMessage();
+
+            MailboxAddress from = new MailboxAddress("Z&S Restoran Rezervasyon", "fatmazehraseren@gmail.com");
+            message.From.Add(from);
+
+            MailboxAddress to = new MailboxAddress("User", cmdto.ReceiverMail);
+            message.To.Add(to);
+
+            var builder = new BodyBuilder();
+            builder.TextBody = cmdto.Body;
+            message.Body = builder.ToMessageBody();
+
+            message.Subject = cmdto.Subject;
+
+            SmtpClient client = new SmtpClient();
+            client.Connect("smtp.gmail.com", 587, false);
+            client.Authenticate("fatmazehraseren@gmail.com", "nmdu ufbp ktqi retd");
+
+            client.Send(message);
+            client.Disconnect(true);
+
+            return RedirectToAction("Index", "Statistic");
+        }
+    }
+}
