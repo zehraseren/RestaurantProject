eba6a85dcc8ab15bef507fa8d3c0b3c5add0d351
Zehra Seren <fatmazehraseren@gmail.com>
2025-02-23 01:13:27 +0300

    Implemented world cuisine meals fetching via API

---
+++ SignalR.WebUI/Controllers/FoodRapidApiController.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.WebUI.Dtos.RapidApiDtos;
+
+namespace SignalR.WebUI.Controllers
+{
+    public class FoodRapidApiController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://tasty.p.rapidapi.com/recipes/list?from=0&size=58&tags=under_30_minutes"),
+                Headers =
+    {
+        { "x-rapidapi-key", "85e0067b92mshaad1b9d110ca2c4p1ffe7fjsn7fea82739ad4" },
+        { "x-rapidapi-host", "tasty.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var root = JsonConvert.DeserializeObject<RootTastyApiDto>(body);
+                var values = root.Results;
+                return View(values);
+            }
+        }
+    }
+}
---
+++ SignalR.WebUI/Dtos/RapidApiDtos/ResultTastyApiDto.cs
@@ -0,0 +1,15 @@
+﻿namespace SignalR.WebUI.Dtos.RapidApiDtos
+{
+    public class RootTastyApiDto
+    {
+        public List<ResultTastyApiDto> Results { get; set; }
+    }
+
+    public class ResultTastyApiDto
+    {
+        public string name { get; set; }
+        public string original_video_url { get; set; }
+        public int total_time_minutes { get; set; }
+        public string thumbnail_url { get; set; }
+    }
+}
